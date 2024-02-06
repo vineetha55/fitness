2e7a15695a32f1e353a69cf83d1d92c8abc37d5f
Vineetha <vineethamonichen44@gmail.com>
2024-02-06 16:46:28 +0530

    third commit

--- fitness/settings.py
+++ fitness/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 STATIC_DIR=os.path.join(BASE_DIR,"static")
 STATICFILES_DIRS=[STATIC_DIR]
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
